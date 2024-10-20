f0acc235f2aeda6e5cbe7b5127acb031184b0eda
NevermindNilas <nilascontact@gmail.com>
2024-10-20 17:12:40 +0300

    test on a video

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import time
+from tqdm import tqdm
 
 
 def calcOpticalFlowFarneback(frame1, frame2, threshold=1.0, percentage=0.95):
@@ -19,6 +20,42 @@ def calcOpticalFlowFarneback(frame1, frame2, threshold=1.0, percentage=0.95):
 
     # Will return a BOOL value, and the prediction value
     return zeroFlowPercentage > percentage, zeroFlowPercentage
+
+
+def countDuplicateFrames(videoPath):
+    cap = cv2.VideoCapture(videoPath)
+    if not cap.isOpened():
+        print("Error: Could not open video.")
+        return
+
+    # totalFrames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    ret, prevFrame = cap.read()
+    if not ret:
+        print("Error: Could not read the first frame.")
+        return
+
+    duplicateCount = 0
+    frameCount = 0
+
+    with tqdm(total=100, desc="Processing frames") as pbar:
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            if frameCount == 100:  # I only really wanna do 100 frames for testing rn
+                break
+
+            frameCount += 1
+            result, _ = calcOpticalFlowFarneback(prevFrame, frame)
+            if result:
+                duplicateCount += 1
+
+            prevFrame = frame
+            pbar.update(1)
+
+    cap.release()
+    print(f"Total frames: {frameCount}")
+    print(f"Duplicate frames: {duplicateCount}")
 
 
 if __name__ == "__main__":
@@ -41,3 +78,6 @@ if __name__ == "__main__":
 
     print(f"Time taken to compare frames: {elapsedTime:.6f} seconds")
     print(f"Estimated FPS: {fps:.2f}")
+
+    videoPath = r"G:\TheAnimeScripter\input\input.mp4"
+    countDuplicateFrames(videoPath)
