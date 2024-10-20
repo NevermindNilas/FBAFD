57e87638a54ac5887cedacfc1210dfc4826352d1
NevermindNilas <nilascontact@gmail.com>
2024-10-20 18:00:21 +0300

    done for now, gotta test memflow later

--- src/pytorchFlowRaft.py
+++ src/pytorchFlowRaft.py
@@ -3,38 +3,39 @@ import torch.nn.functional as F
 from torchvision.models.optical_flow import raft_large
 from torchvision.models.optical_flow import Raft_Large_Weights
 
+# TO:DO TWEAK THAT PERCENTAGE STUFF
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Load the pre-trained RAFT model once
+weights = Raft_Large_Weights.DEFAULT
+raftModel = raft_large(weights=weights).to(device).eval()
+
+
+def prepareTensor(frame, precision):
+    tensor = torch.from_numpy(frame).permute(2, 0, 1).unsqueeze(0).to(device) / 255.0
+    if precision == "fp16":
+        tensor = tensor.half()
+    else:
+        tensor = tensor.float()
+    tensor = F.interpolate(
+        tensor, size=(520, 960), mode="bilinear", align_corners=False
+    )
+    return tensor
 
 
 @torch.inference_mode()
 def pytorchFlowRaft(
-    frame1, frame2, threshold=1.0, percentage=0.95, precision: str = "fp32"
+    frame1, frame2, threshold=1.0, percentage=0.99999, precision: str = "fp32"
 ):
-    tensor1 = torch.from_numpy(frame1).permute(2, 0, 1).unsqueeze(0).to(device) / 255.0
-    tensor2 = torch.from_numpy(frame2).permute(2, 0, 1).unsqueeze(0).to(device) / 255.0
-
-    # Load the pre-trained RAFT model
-    weights = Raft_Large_Weights.DEFAULT
-    raft_model = raft_large(weights=weights).to(device).eval()
+    tensor1 = prepareTensor(frame1, precision)
+    tensor2 = prepareTensor(frame2, precision)
 
     if precision == "fp16":
-        raft_model.half()
-        tensor1 = tensor1.half()
-        tensor2 = tensor2.half()
+        raftModel.half()
     else:
-        raft_model.float()
-        tensor1 = tensor1.float()
-        tensor2 = tensor2.float()
+        raftModel.float()
 
-    # Requires frames of size 520x960, so resize the frames
-    tensor1 = F.interpolate(
-        tensor1, size=(520, 960), mode="bilinear", align_corners=False
-    )
-    tensor2 = F.interpolate(
-        tensor2, size=(520, 960), mode="bilinear", align_corners=False
-    )
-
-    flow = raft_model(tensor1, tensor2)[-1]
+    flow = raftModel(tensor1, tensor2)[-1]
     magnitude = torch.sqrt(flow[:, 0, :, :] ** 2 + flow[:, 1, :, :] ** 2)
 
     # Apply threshold to the magnitude of the flow vectors
